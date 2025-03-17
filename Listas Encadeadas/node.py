5dbd495acaf0efc2344227098e8f7bdb087bb1f0
Samantha Falcão <samanthafalcao.2018@gmail.com>
2025-03-17 20:28:08 -0300

    node.py

--- Listas Encadeadas/node.py
+++ Listas Encadeadas/node.py
@@ -1,1 +1,5 @@
-
+class Node:
+    """Classe que representa um nó da lista encadeada."""
+    def __init__(self, data):
+        self.data = data  # Dado armazenado no nó
+        self.next = None  # Referência para o próximo nó

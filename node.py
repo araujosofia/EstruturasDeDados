12284226b27ea3164ee05f7cf818d88c2cb26fc0
Samantha Falcão <samanthafalcao.2018@gmail.com>
2025-03-17 20:21:24 -0300

    Delete node.py

--- node.py
+++
@@ -1,5 +0,0 @@
-cclass Node:
-    """Classe que representa um nó da lista encadeada."""
-    def __init__(self, data):
-        self.data = data  # Dado armazenado no nó
-        self.next = None  # Referência para o próximo nó

4696b8d9bb9937e17786f482c32158da66df5343
araujosofia <142227976+araujosofia@users.noreply.github.com>
2025-04-29 14:49:48 -0300

    Update Atividade02.py

--- Atividades/Vetor e Matriz/Atividade02.py
+++ Atividades/Vetor e Matriz/Atividade02.py
@@ -1,1 +1,14 @@
+# 2) Matriz de caracteres 4x4
 
+# Criando a matriz com compreensão de lista
+letras = [chr(i) for i in range(ord('a'), ord('p') + 1)]
+
+# Preenchendo a matriz 4x4
+matriz = [letras[i:i+4] for i in range(0, 16, 4)]
+
+# Exibindo a matriz com índices de linha e coluna
+print("\nMatriz de caracteres 4x4:")
+for i in range(4):
+    for j in range(4):
+        print(f"{matriz[i][j]}", end=' ')
+    print()
